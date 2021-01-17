dd28278e8cd628f4621beefa5cf9ca966aad6223
Stewart Newnham <git@newnham.com>
2021-01-17 22:29:59 +0800

    Project To Show the Use of Props in Components

---
+++ 01-pokemon/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import Pokecard from './components/pokecard/Pokecard.component';
+// import Pokedex from './components/pokedex/Pokedex.component';
+import Pokegame  from './components/Pokegame/Pokegame.component';
+
+function App() {
+  return (
+    <div className="App">
+        <Pokegame />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-pokemon/src/components/pokecard/Pokecard.component.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+import './Pokecard.css'
+
+// const POKE_API ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"; 
+const POKE_API ="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"; 
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number ); // slice last 3 numbers
+
+class Pokecard extends Component {
+
+
+
+    render(){
+        const { id, name, type, exp }  = this.props;
+        let imgSrc = `${POKE_API}${padToThree(id)}.png`
+        return (
+            <div className="Pokecard">
+                <h1 className="Pokecard-title">{name}</h1>
+                <div className="Pokecard-image">
+                    <img src={imgSrc} alt={name}/>
+                </div>
+                <div className="Pokecard-data">ID: {id}</div>
+                <div className="Pokecard-data">Type: {type}</div>
+                <div className="Pokecard-data">Experience: {exp}</div>
+            </div>
+        );
+    }
+}
+
+export default Pokecard;
