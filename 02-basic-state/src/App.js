80e480281144fcbd87fb925d8827c42edad56e30
Stewart Newnham <git@newnham.com>
2021-01-24 13:05:54 +0800

    Understanding State

---
+++ 02-basic-state/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Clicker from './Clicker.component';
+function App() {
+  return (
+    <div className="App">
+      <Clicker />
+    </div>
+  );
+}
+
+export default App;
